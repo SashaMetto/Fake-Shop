f534546684bbf04165167a053a6f9fdde3ec0a14
Sasha Metto <sasha.metto@gmail.com>
2022-05-07 22:06:34 +0300

    test shoes section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
+import search from "./images/search.webp"
+import profile from "./images/profile.webp"
+import cart from "./images/cart.png"
 
 export default function App() {
   return (
     <div className="App">
       
       <nav className="nav">
-      <h1 className="nav nav_header">Fake store</h1>
-        <Link to="/">Home</Link>
-        <Link to="/cat1">Cat1</Link>
-        <Link to="/cat2">Cat2</Link>
-        <Link to="/cat3">Cat3</Link>
-        <Link to="/gall">Gallery</Link>
-        <Link to="/contact">Contact</Link>
+      <h1 className="nav__header">Fake store</h1>
+      <div className="nav__links">  
+        <Link className="nav__link" to="/">Home</Link>
+        <Link className="nav__link" to="/cat1">Shoes</Link>
+        <Link className="nav__link" to="/cat2">Cat2</Link>
+        <Link className="nav__link" to="/cat3">Cat3</Link>
+        <Link className="nav__link" to="/gall">Gallery</Link>
+        <Link className="nav__link" to="/contact">Contact</Link>
+        </div>
+        <div className="nav__search__profile__currency__cart">
+        <input></input>
+        <img className="nav__searchbutton nav___medium" alt="search" src={search}></img>
+        <img className="nav__profilebutton nav___medium" alt="profile" src={profile}></img>
+        <img className="nav__cartbutton nav___medium" alt="profile" src={cart}></img>
+        </div>
       </nav>
       <Outlet />
     </div>
--- src/routes/cat1.jsx
+++ src/routes/cat1.jsx
@@ -1,7 +1,53 @@
+import * as React from "react";
+import {
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useSearchParams,
+  useParams,
+} from "react-router-dom";
+import { LinkProps } from "react-router-dom";
+import {sneakers, getSneakerById} from "../shoes.jsx"
 export default function Cat1() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2>Expenses1</h2>
+        <h2>Shoes</h2>
+        <p>A shoe is an item of footwear intended to protect and comfort the human foot. Shoes are also used as an item of decoration and fashion.</p>
+        <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+          gap: "12px 24px",
+        }}
+      >
+        {sneakers.map((snkr) => {
+          let name = `${snkr.brand} ${snkr.model}`;
+          let color = snkr.colorway;
+          let price = snkr.price;
+          return (
+            <div key={snkr.id} style={{ position: "relative" }}>
+              <img
+                width={200}
+                height={200}
+                src={snkr.imageUrl}
+                alt={name}
+                style={{
+                  borderRadius: "8px",
+                  width: "100%",
+                  height: "auto",
+                  aspectRatio: "1 / 1",
+                }}
+              />
+              <div>
+                <p>{name}</p>
+                <p>{color}</p>
+                <p>{price} <button>Add to cart</button></p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       </main>
     );
   }
